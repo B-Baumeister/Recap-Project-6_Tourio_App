672c66c66b5c369f2d926fc5a848afb01720b0d3
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-09-17 20:43:58 +0200

    update entry

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -5,13 +5,23 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
+  if (!id) {
+    return;
+  }
+
   if (request.method === "GET") {
     const place = await Place.findById(id);
-
     if (!place) {
       return response.status(404).json({ status: "Not found" });
     }
-
     response.status(200).json(place);
+  } else if (request.method === "PUT") {
+    const placeData = request.body;
+
+    await Place.findByIdAndUpdate(id, placeData);
+    // Find the joke by its ID and update the content that is part of the request body!
+    response.status(200).json({ status: `Place ${id} updated!` });
+    // If successful, you'll receive an OK status code.
   }
+  console.log("Place edited (but not really...");
 }
